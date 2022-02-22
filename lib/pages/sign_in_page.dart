d4eb6e98fd3d818a476fa49b5ab7ebccfc48b0d3
Ardiansahhh <ard5070@gmail.com>
2022-02-22 07:20:54 +0700

    menambahkan field email input dan menambahkan defaultmargin pada file theme

--- lib/pages/sign_in_page.dart
+++ lib/pages/sign_in_page.dart
@@ -1,14 +1,94 @@
 import 'package:flutter/material.dart';
+import 'package:shamo/theme.dart';
 
 class SignIn extends StatelessWidget {
-  const SignIn({ Key? key }) : super(key: key);
+  const SignIn({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    Widget header() {
+      return Container(
+        margin: EdgeInsets.only(top: 30.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Login",
+              style: primaryTextStyle.copyWith(
+                fontSize: 24,
+                fontWeight: semiBold,
+              ),
+            ),
+            SizedBox(height: 2),
+            Text(
+              'Sign In to continue',
+              style: subtitleTextStyle,
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget emailInput() {
+      return Container(
+        margin: EdgeInsets.only(top: 70),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              "Email Address",
+              style:
+                  primaryTextStyle.copyWith(fontSize: 16, fontWeight: medium),
+            ),
+            SizedBox(height: 12),
+            Container(
+              height: 50,
+              padding: EdgeInsets.symmetric(
+                horizontal: 16,
+              ),
+              decoration: BoxDecoration(
+                color: bgColor2,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Center(
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Image.asset(
+                      'assets/icons/icon_email.png',
+                      width: 17.0,
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: TextFormField(
+                        style: primaryTextStyle,
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Your email address',
+                          hintStyle: subtitleTextStyle,
+                          fillColor: primaryColor
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      );
+    }
+
     return Scaffold(
-      body: Center(
-        child: Text('Sign in Page'),
-      ),
-    );
+        backgroundColor: bgColor1,
+        body: SafeArea(
+          child: Container(
+            margin: EdgeInsets.symmetric(horizontal: defaultMargin),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [header(), emailInput()],
+            ),
+          ),
+        ));
   }
 }
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+double defaultMargin = 30.0;
 Color primaryColor     = Color(0xFF6C5ECF);
 Color secondaryColor   = Color(0xFF38ABBE);
 Color alertColor       = Color(0xFFED6363);
@@ -10,6 +11,7 @@ Color bgColor2         = Color(0xFF2B2937);
 Color bgColor3         = Color(0xFF242231);
 Color primaryTextColor = Color(0xFFF1F0F2);
 Color secondTextColor  = Color(0xFF999999);
+Color subtitleColor    = Color(0xFF504F5E);
 
 TextStyle primaryTextStyle = GoogleFonts.poppins(
   color: primaryTextColor,
@@ -21,6 +23,10 @@ TextStyle secondTextStyle = GoogleFonts.poppins(
 
 TextStyle priceTextStyle = GoogleFonts.poppins(
   color: priceColor,
+);
+
+TextStyle subtitleTextStyle = GoogleFonts.poppins(
+  color: subtitleColor,
 );
 
 FontWeight light    = FontWeight.w300;
