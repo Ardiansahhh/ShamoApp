bf17a622afa4fff5257372f10381c25c03620b81
Ardiansahhh <ard5070@gmail.com>
2022-02-28 17:23:45 +0700

    menambahkan header profile di profile page

--- lib/home/profile_page.dart
+++ lib/home/profile_page.dart
@@ -2,13 +2,54 @@ import 'package:flutter/material.dart';
 import '../theme.dart';
 
 class ProfilePage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text("Profile Page"),
-      ),
+    Widget header() {
+      return AppBar(
+        backgroundColor: bgColor1,
+        automaticallyImplyLeading: false,
+        elevation: 0,
+        flexibleSpace: SafeArea(
+            child: Container(
+          padding: EdgeInsets.all(defaultMargin),
+          child: Row(
+            children: [
+              ClipOval(
+                child: Image.asset(
+                  'assets/icons/icon_image_profile.png',
+                  width: 64,
+                ),
+              ),
+              SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Hello, Katarina',
+                      style: primaryTextStyle.copyWith(
+                          fontSize: 24, fontWeight: semiBold),
+                    ),
+                    Text(
+                      '@katrine',
+                      style: subtitleTextStyle.copyWith(
+                        fontSize: 16,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Image.asset('assets/icons/icon_exit.png', width: 20)
+            ],
+          ),
+        )),
+      );
+    }
+
+    return Column(
+      children: [
+        header(),
+      ],
     );
   }
 }
